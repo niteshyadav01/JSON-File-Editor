1ac272407d7f5a61c7fa2ae27d06d55675279d5f
niteshyadav01 <nitesh.yadavvv07@gmail.com>
2026-09-10 11:05:38 +0530

    Revert "update the folder structure"
    This reverts commit a5e3215b091e25e4030ced3c71ba37720b4c66aa.

--- src/gui/win_main.py
+++ src/gui/win_main.py
@@ -1,45 +1,43 @@
-from PySide6.QtCore import QCoreApplication, QMetaObject, QRect
-from PySide6.QtGui import QAction
-from PySide6.QtWidgets import (
-    QMainWindow,
-    QMenu,
-    QMenuBar,
-    QStatusBar,
-    QTreeView,
-    QVBoxLayout,
-    QWidget,
-)
 
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QMainWindow, QMenu,
+    QMenuBar, QSizePolicy, QStatusBar, QTreeView,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(800, 600)
-
         self.actionopen = QAction(MainWindow)
         self.actionopen.setObjectName(u"actionopen")
         self.actionedit = QAction(MainWindow)
         self.actionedit.setObjectName(u"actionedit")
         self.actionSave_As = QAction(MainWindow)
         self.actionSave_As.setObjectName(u"actionSave_As")
-
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.listView = QTreeView(self.centralwidget)
         self.listView.setObjectName(u"listView")
-        self.verticalLayout.addWidget(self.listView)
-        MainWindow.setCentralWidget(self.centralwidget)
 
+        self.verticalLayout.addWidget(self.listView)
+
+        MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 800, 33))
         self.menuFile = QMenu(self.menubar)
         self.menuFile.setObjectName(u"menuFile")
         MainWindow.setMenuBar(self.menubar)
-
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -50,21 +48,15 @@ class Ui_MainWindow(object):
         self.menuFile.addAction(self.actionSave_As)
 
         self.retranslateUi(MainWindow)
+
         QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(
-            QCoreApplication.translate("MainWindow", u"MainWindow", None)
-        )
-        self.actionopen.setText(
-            QCoreApplication.translate("MainWindow", u"Upload", None)
-        )
-        self.actionedit.setText(
-            QCoreApplication.translate("MainWindow", u"Save", None)
-        )
-        self.actionSave_As.setText(
-            QCoreApplication.translate("MainWindow", u"Save As", None)
-        )
-        self.menuFile.setTitle(
-            QCoreApplication.translate("MainWindow", u"File", None)
-        )
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionopen.setText(QCoreApplication.translate("MainWindow", u"Upload", None))
+        self.actionedit.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+        self.actionSave_As.setText(QCoreApplication.translate("MainWindow", u"Save As", None))
+        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
+    # retranslateUi
+
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from models.edit_model import MainWindow
+from gui.tree_model import MainWindow
 
 
 def main():
